8ec26195e9ffaa7aa6ad61d443e7643832f9a708
Podmno <podmno@icloud.com>
2024-03-21 16:03:24 +0800

    update common header

--- src/utility/common_ftp.rs
+++ src/utility/common_ftp.rs
@@ -8,24 +8,31 @@ use crate::utility::common_header::GUCommonFTPDevice;
 
 pub struct GUCommonFTPClient {
 
-    server_info: GUCommonFTPDevice
+    server_info: Option<GUCommonFTPDevice>
 }
 
 impl GUCommonFTPClient {
     fn new() -> Self {
         Self {
-            server_info: todo!(),
+            server_info: None
         }
     }
 
     /// stup with GUCommonFTPDevice struct
-    fn setup_server_info(device_info: GUCommonFTPDevice) {
-        print!("Common FTP > setup_server_info : target FTP Address > {}",device_info.device_ftp_address)
+    fn setup_server_info(&mut self,device_info: GUCommonFTPDevice) {
+        print!("Common FTP > setup_server_info : target FTP Address > {}",device_info.device_ftp_address);
+        self.server_info = Some(device_info);
+    }
 
+    fn test_connection(&self) {
 
     }
 
-    fn test_connection() {
+    fn get_file_list() {
 
+    }
+
+    fn get_file_info() {
+        
     }
 }
--- src/utility/common_header.rs
+++ src/utility/common_header.rs
@@ -1,4 +1,20 @@
+/*
+
+    Utility Common Header
+
+*/
+
+
 pub struct GUCommonFTPDevice {
     pub device_ftp_address: String,
     pub device_ftp_port: i32,
+}
+
+impl GUCommonFTPDevice {
+    fn new() -> Self {
+        Self {
+            device_ftp_address: "".to_string(),
+            device_ftp_port: 0
+        }
+    }
 }
